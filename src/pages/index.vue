3265935547dd96ab814455668a7ad2be4a46c976
gyp <826256407@qq.com>
2023-03-27 16:39:03 +0800

    更新ui

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,7 +3,7 @@
   <div  class="pg-index">
     <div class="index-container">
       <img width="300px" src="../assets/img/xm.jpg">
-      <p>一款基于Vue.js轻量级、模块化的 思迈特KA UI 组件库</p>
+      <p>一款基于Vue.js轻量级、模块化的 思迈特 Smartbi KA UI 组件库</p>
     </div>
     <div class="separate-panel">
       <div class="about">
